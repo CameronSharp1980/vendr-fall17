38cd76719e52c452f7017207600704fb5baa7709
Cameron Sharp <cameron.sharp1980@gmail.com>
2017-10-30 17:47:53 -0600

    Added change and pop types

--- app/vend/vend-controller.js
+++ app/vend/vend-controller.js
@@ -1,20 +1,35 @@
-function VendController(){
+function VendController() {
 
     // PRIVATE PART
     var vendService = new VendService() //CONSTRUCTORS RETURN OBJECTS
-    
     var balanceElem = document.getElementById('current-balance')
-    
-    function update(){
+    var dewElem = document.getElementById("mt-dew-pic")
+    var changeElem = document.getElementsByClassName("change-span")[0]
+
+    function update() {
         balanceElem.innerText = vendService.getBalance()
+        // changeElem.innerText = vendService.makeChange()
     }
 
     // PUBLIC PARTS
-    this.buyMtDew = function buyMtDew(){
-        console.log('DID IT WORK?')
+    this.buyPop = function buyPop(type) {
+        console.log('buyPop Start')
+        console.log(type)
+        if (vendService.buyPop(type)) {
+            console.log("Do the Dew")
+            changeElem.innerText = vendService.makeChange()
+            update()
+            document.getElementById("mt-dew-pic").classList.remove("hidden")
+        } else {
+            console.log("Feed me more quarters")
+            update()
+        }
     }
+    update();
 
-    this.addQuarter = function addQuarter(){
+
+    this.addQuarter = function addQuarter() {
+        document.getElementById("mt-dew-pic").classList.add("hidden")
         vendService.addQuarter()
         update()
     }
--- app/vend/vend-service.js
+++ app/vend/vend-service.js
@@ -1,22 +1,47 @@
-function VendService(){
+function VendService() {
 
     // PRIVATE PARTS
     var machineTotal = 5
     var balance = 0
-    var mtDewCost = 1
+    var pops = {
+        dew: {name: "Mountain Dew", price: 1},
+        pepsi: {name: "Pepsi", price: 1.25}
+    }
+    // var mtDewCost = 1
     var quarterValue = .25
+    var change = 0;
+
 
     // PUBLIC PARTS
     // GETTERS AND SETTERS
-    this.getBalance = function getBalance(){
+    this.getBalance = function getBalance() {
         return balance
     }
 
-    this.addQuarter = function addQuarter(){
+    this.addQuarter = function addQuarter() {
         balance += quarterValue
+        machineTotal += quarterValue
+        console.log("Balance: " + balance)
+        console.log("Machine Total: " + machineTotal)
     }
 
-    
+    this.makeChange = function makeChange() {
+        change = balance;
+        balance = 0;
+        console.log("Change" + change);
+        console.log("Balance" + balance);
+        return change;
+    }
+
+    this.buyPop = function buyPop(type) {
+        if (balance >= pops[type].price) {
+            balance -= pops[type].price;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
 
 }
